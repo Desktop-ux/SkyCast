b8c56a41186dd1823178a12a1329499c9fba4070
Ayaan <desktop.ux@gmail.com>
2025-08-21 23:31:02 +0530

    Api Updated to https

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function hidinterrupt(){
 }
 
 function getinfo(country) {
-  return fetch(`http://api.weatherapi.com/v1/forecast.json?key=630eb0036eb2406bab5163749252108&q=${country}&days=1&aqi=no&alerts=no`).then((raw) => {
+  return fetch(`https://api.weatherapi.com/v1/forecast.json?key=630eb0036eb2406bab5163749252108&q=${country}&days=1&aqi=no&alerts=no`).then((raw) => {
     return raw.json()
   })
 }
